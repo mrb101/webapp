1c639e8bbb24ae5ce8757535d2836ba93acaf468
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2015-09-18 21:49:31 +0800

    adds adduser as a command line

--- manage.py
+++ manage.py
@@ -12,6 +12,21 @@ manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
+@manager.command
+def adduser(email, username, admin=False):
+    """Register a new User"""
+    from getpass import getpass
+    password = getpass()
+    password2 = getpass(prompt='Confirm: ')
+    if password != password2:
+        import sys
+        sys.exit('Error: Password do not match')
+    db.create_all()
+    user = User(email=email, password=password, admin=admin)
+    db.session.add(user)
+    db.session.commit()
+    print('User {0} was successfully created'.formate(username))
+
 
 if __name__ == '__main__':
     manager.run()
