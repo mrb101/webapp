3d0a71f0c3f1ec8d61bd22d905967b196345f355
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2015-09-18 21:55:22 +0800

    alters table users add admin option

--- app/models.py
+++ app/models.py
@@ -19,6 +19,7 @@ class User(db.Model):
     user_name = db.Column(db.String(20), nullable= False, unique=True, index=True)
     email = db.Column(db.String(255), nullable=False, unique=True, index=True)
     password_hash = db.Column(db.String(255))
+    admin = db.Column(db.Boolean)
     created_at = db.Column(db.DateTime(), default=datetime.utcnow)
 
     @property
