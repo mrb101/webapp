f99973ed766bea2777e1316a48798fdeb004d63e
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2015-09-18 20:12:03 +0800

    adds uses table

--- app/models.py
+++ app/models.py
@@ -1,11 +1,21 @@
 from datetime import datetime
 
 # import the db from main
-from . import db
+from app import db
 
 
 # Models goes here....
 class _module1(db.Model):
     __tablename__ = '_module1'
-    id = db.Column(db.intger, primary_key=True)
-    created_at = db.Column(db.Datetime(), default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime(), default=datetime.utcnow)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    user_name = db.Column(db.String(20), nullable= False, unique=True, index=True)
+    email = db.Column(db.String(255), nullable=False, unique=True, index=True)
+    password_hash = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime(), default=datetime.utcnow)
