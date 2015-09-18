3cf5c37d93f5b3c3600a07b71cf7f52bfa775776
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2015-09-18 23:33:52 +0800

    adds initial login route

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,10 +1,13 @@
 from flask import render_template
 from . import auth
+from .forms import LoginForm
 
-
-@auth.route('/signin')
+@auth.route('/signin', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/signin.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('auth/signin.html', form=form)
 
 
 @auth.route('/signup')
