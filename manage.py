5bf173063bdfe66097807e2c28ea3837420fb97b
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2015-09-18 20:11:29 +0800

    adds migrations with flask-migrate commands

--- manage.py
+++ manage.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 import os
-from app import create_app
+from app import create_app, db
 from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
 manager = Manager(app)
+
+
+manager.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
