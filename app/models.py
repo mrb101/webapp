47fcccb9fb4258cbb319068a3ea2fe6eabe2f327
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2015-09-18 21:50:08 +0800

    creates password hash functions

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # import the db from main
 from app import db
@@ -19,3 +20,14 @@ class User(db.Model):
     email = db.Column(db.String(255), nullable=False, unique=True, index=True)
     password_hash = db.Column(db.String(255))
     created_at = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_passowrd(self, password):
+        return check_password_hash(self.password_hash, password)
