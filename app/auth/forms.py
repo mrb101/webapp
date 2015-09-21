d72f5e0917fc539fd24a088a03780e1ee7d35a4c
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2015-09-21 10:03:59 +0800

    adds profile form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -16,3 +16,11 @@ class RegisterForm(Form):
     password = PasswordField('Password', validators=[Required()])
     password = PasswordField('Password', validators=[Required()])
     submit = SubmitField('Sign In')
+
+
+class ProfileForm(Form):
+    name = StringField('Name', validators=[Required(), Length(1, 255)])
+    user_name = StringField('UserName', validators=[Required(), Length(1, 255)])
+    email = StringField('Email', validators=[Required(), Length(1, 255), Email()])
+    bio = StringField('Bio', validators=[Length(1, 255)])
+    submit = SubmitField('Update')
