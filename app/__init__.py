17784843e342dd226da5af81ce9d99ea1ae19f2d
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2015-09-21 12:50:58 +0800

    registers inventory blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -63,4 +63,7 @@ def create_app(config_name):
     from .notifications import notifications as notifications_blueprint
     app.register_blueprint(notifications_blueprint, url_prefix='/notifications')
 
+    from .inventory import inventory as inventory_blueprint
+    app.register_blueprint(inventory_blueprint, url_prefix='/inventory')
+
     return app
