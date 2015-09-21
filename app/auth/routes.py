00b49a5a9eaf305e2d642294ef871afb39e6bc56
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2015-09-21 10:07:26 +0800

    adds profile route

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, request, redirect, url_for, flash
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm, ProfileForm
 from ..models import User
-from flask.ext.login import login_user, logout_user, login_required
+from flask.ext.login import login_user, logout_user, login_required, current_user
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -13,7 +13,7 @@ def login():
             flash('Invalid email or password.')
             return redirect(url_for('auth.login'))
         login_user(user, form.remember_me.data)
-        return redirect(request.args.get('next') or url_for('auth.login'))
+        return redirect(request.args.get('next') or url_for('auth.profile'))
     return render_template('auth/login.html', form=form)
 
 
@@ -35,6 +35,21 @@ def settings():
     return render_template('auth/settings.html')
 
 
-@auth.route('/profile')
+@auth.route('/profile', methods=['GET', 'POST'])
+@login_required
 def profile():
-    return render_template('auth/profile.html')
+    form = ProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.user_name = form.name.data
+        current_user.email = form.email.data
+        current_user.bio = form.bio.data
+        db.session.add(current_user._get_current_object())
+        db.session.commit()
+        flash('Your profile has been updated successfully!')
+        return redirect(url_for('auth.profile', user_name=current_user.user_name))
+    form.name.data = current_user.name
+    form.user_name.data = current_user.user_name
+    form.email.data = current_user.email
+    form.bio.data = current_user.bio
+    return render_template('auth/profile.html', form=form)
